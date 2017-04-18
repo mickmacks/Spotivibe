39c4771d66f842871b853b4334675e8f703791f9
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-04-18 12:03:52 -0700

    Initiated Project. Full Working Tree

---
+++ server.js
@@ -0,0 +1,47 @@
+// SERVER-SIDE JAVASCRIPT // 
+
+/****************
+ * REQUIREMENTS *
+ ****************/
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var controllers = require('./controllers');
+
+
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+/*
+ * JSON API Endpoints
+ */
+
+// app.get('/api', controllers.api.index);
+
+
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
