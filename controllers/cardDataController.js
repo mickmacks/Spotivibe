ff7f0c6e121f21d5ce3e079a2f91af72f8f80fe3
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-18 16:59:03 -0700

    controllers and server.js fill ed. created  controllers/cardDataController.js

---
+++ controllers/cardDataController.js
@@ -0,0 +1,23 @@
+/************
+ * DATABASE *
+ ************/
+
+var db = require('../models');
+
+
+// GET /api/cards
+function index(req, res) {
+  // send back all albums as JSON
+  db.Card.find({}, function(err, allCards) {
+    res.json(allACards);
+  }
+
+
+  // export public methods here
+  // module.exports = {
+  //   index: index,
+  //   create: create,
+  //   show: show,
+  //   destroy: destroy,
+  //   update: update
+  // };
---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+// these names match with server.js
+
+module.exports = {
+  api: require('./apiController'),
+  cardsData: require('./cardsDataController'),
+  // albumsSongs: require('./albumsSongscontroller')
+}
