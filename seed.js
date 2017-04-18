247e896effacf050f7d83f44f6a37c0fe767c303
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-18 15:11:44 -0700

    has seed.js with practice data

---
+++ seed.js
@@ -0,0 +1,31 @@
+// This file allows us to seed our application with data
+// simply run: `node seed.js` from the root of this project folder.
+
+var db = require("./models");
+
+var cardSeed =[];
+cardSeed.push({
+  playlistName: "Moody!",
+  playlistLink: "URL.dude",
+  genre: "wild",
+  artistNames: ["Tivo", "Doug"],
+  owner: Mike
+});
+cardSeed.push({
+  playlistName: "Smoooothe",
+  playlistLink: "URL.dude2",
+  genre: "jazz",
+  artistNames: ["Blue Lights"],
+  owner: "Robert Redford"
+});
+
+db.Card.remove({}, function(err, cards){
+
+  db.Card.create(cardEach, function(err, cards){
+    if (err) { return console.log('ERROR', err); }
+    console.log("all cards:", cards);
+    // console.log("created", albums.length, "albums");
+    process.exit();
+  });
+
+});
