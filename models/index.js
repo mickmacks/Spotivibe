416e168ea7a94b3aea0d5fcde2e045a6b6db8e2e
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-19 18:35:08 -0700

    models/index.js setup to go to heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/spotify");
+// mongoose.connect("mongodb://localhost/spotify");
 // for heroku got to change above line
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/spotify" );
 
 var Genre = require('./genre');
 
