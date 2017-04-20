9ba81475fb4cf4ebb7318f923768878f30bea89d
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-19 19:15:15 -0700

    model/index change for heroku/mongo connection

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 var mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost/spotify");
 // for heroku got to change above line
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/spotify" );
+mongoose.connect( "mongodb://heroku_vtdsxzk7:ejqjn63i99i8k2epupm8iueafh@ds111441.mlab.com:11441/heroku_vtdsxzk7" || "mongodb://localhost/spotify" );
+//  mongodb://heroku_vtdsxzk7:ejqjn63i99i8k2epupm8iueafh@ds111441.mlab.com:11441/heroku_vtdsxzk7
 
 var Genre = require('./genre');
 
