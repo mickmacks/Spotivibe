8badc3796c94284b4549ed5e2513cb5605e89572
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-04-20 16:02:00 -0700

    preparing for new master pull

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,8 +19,10 @@ $(document).ready(function() {
   //form select dropdown animation
   $('select').material_select();
 
-  // CREATE
+  // EDIT
   // catch and handle the click on an add playlist button event
+  $('#activities').on('click', '#edit-button', handleEditActivityClick);
+
   $('#activity-form').submit(function(e) {
 
     e.preventDefault();
@@ -38,16 +40,16 @@ $(document).ready(function() {
   });
 
   //DELETE
-  $('#activities').on('click', '.mike_test_delete', handleDeleteCardClick);
+  $('#activities').on('click', '#card-delete-button', handleDeleteCardClick);
 
-});  //  $(document).ready
+});
 
 ////////////////////////
 // DELETE ACTIVITY CARDS
 ////////////////////////
 
-function handleDeleteCardClick(e)
-  {
+function handleDeleteCardClick(e) {
+  e.preventDefault();
   var id = $(this).closest('.activity-card').data('card-id');  //  '.data('card-id')' is same as '<div data-card-id=' below.
   console.log(id);
   // $('div[data-album-id=' + deletedAlbumId + ']').remove();
@@ -69,13 +71,51 @@ function handleDeleteCardSuccess(data) {
 
 
 ////////////////////////
+// EDIT ACTIVITY CARDS
+////////////////////////
+
+function handleEditActivityClick(e) {
+
+  e.preventDefault();
+
+  var $card = $(this).closest('.activity-card');
+  var $cardId = $card.data('card-id');
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/cards/' + $cardId,
+    success: populateEditForm
+  });
+
+}
+
+function populateEditForm(data) {
+
+  console.log(data);
+
+  console.log(data.playlistName);
+
+  var $modal = $('#modal1');
+
+  $modal.find('#playlistName').val("" + data.playlistName);
+  
+  $modal.find('#genreSelect').val("" + data.genre);
+  $modal.find('#playlistLink').val("" + data.playlistLink);
+  $modal.find('#artistNames').val("" + data.artistNames);
+  $modal.find('#owner').val("" + data.owner);
+
+
+}
+
+
+////////////////////////
 // READ ACTIVITY CARDS
 ////////////////////////
 
 // function parameter = database return object 'card'
 function renderMultipleActivities(activities) {
 
-  console.log(activities)
+  console.log(activities);
 
   activities.forEach(function(activity) {
     renderActivity(activity);
@@ -93,11 +133,10 @@ function renderActivity(activity) {
 
           <h3 class="left-align">${activity.playlistName}</h3>
           <h5 class="left-align">${activity.genre}</h5>
-          <button type="button" class="mike_test_delete">Delete card</d>
 
         </div>
 
-        <div><i class="material-icons large play-icon">play_circle_filled</i></div>
+        <div><i class="material-icons medium play-icon">play_arrow</i></div>
 
         <div class="card-data">
 
@@ -106,6 +145,13 @@ function renderActivity(activity) {
 
             <h6 class="left-align">Created By</h6>
             <h4 class="left-align">${activity.owner}</h4>
+            
+            <div class="col s12 center">
+            
+            <a href="#modal1" id="edit-button"><h6>EDIT</h6></a> 
+            <a href="#" id="card-delete-button"><h6>DELETE</h6></a> 
+
+            </div>
 
         </div>
 
